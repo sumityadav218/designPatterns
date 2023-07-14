4917c65fb4c48563b0249cef598d851965981535
sumityadav218 <sumityadav218>
2023-07-14 19:12:05 +0530

    singleton example added

---
+++ src/singletonRealExample/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package singletonRealExample;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    
+    private DatabaseConnection() {
+        // Private constructor to prevent instantiation from outside the class
+    }
+    
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            synchronized (DatabaseConnection.class) {
+                if (instance == null) {
+                    instance = new DatabaseConnection();
+                }
+            }
+        }
+        return instance;
+    }
+    
+    public void connect() {
+        // Code to establish a database connection
+        System.out.println("Connected to the database");
+    }
+    
+    public void disconnect() {
+        // Code to disconnect from the database
+        System.out.println("Disconnected from the database");
+    }
+    
+    // Other methods and variables...
+}
+
---
+++ src/singletonRealExample/MyApp.java
@@ -0,0 +1,15 @@
+package singletonRealExample;
+public class MyApp {
+    public static void main(String[] args) {
+        // Get the singleton instance
+        DatabaseConnection connection = DatabaseConnection.getInstance();
+        
+        // Connect to the database
+        connection.connect();
+        
+        // Perform database operations...
+        
+        // Disconnect from the database
+        connection.disconnect();
+    }
+}
